a8fbc9de957c368ff3112d15d36822f648f05057
AndrejaTorkar <104751136+AndrejaTorkar@users.noreply.github.com>
2023-12-30 17:45:41 +0100

    menu screen bug fixed

--- App.js
+++ App.js
@@ -29,8 +29,12 @@ let restaurantData = [
     pricing: "$",
     img: casinoImage,
     menu: [
-      { category: "Pasta", items: [{ title: "Aglio e Olio" }, { title: "Carbonara" }] },
-      { category: "Ice cream", items: [{ title: "Chocolate" }, { title: "Vanillia" }] },
+      { category: "Pasta", items: [{ title: "Spaghetti Bolognese" }, { title: "Carbonara" }, { title: "Pesto Linguine" }, { title: "Lasagna" }] },
+      { category: "Desserts", items: [{ title: "Tiramisu" }, { title: "Crème Brûlée" }, { title: "Apple Strudel" }, { title: "Chocolate Mousse" }] },
+      { category: "Risotto", items: [{ title: "Mushroom Risotto" }, { title: "Seafood Risotto" }, { title: "Asparagus Risotto" }] },
+      { category: "French Cuisine", items: [{ title: "Coq au Vin" }, { title: "Ratatouille" }, { title: "Boeuf Bourguignon" }] },
+      { category: "Spanish Tapas", items: [{ title: "Patatas Bravas" }, { title: "Gambas al Ajillo" }, { title: "Chorizo a la Sidra" }] },
+      { category: "German Classics", items: [{ title: "Sauerbraten" }, { title: "Wiener Schnitzel" }, { title: "Bratwurst with Sauerkraut" }] },
     ],
   },
   {
@@ -41,8 +45,8 @@ let restaurantData = [
     pricing: "$",
     img: truckersImage,
     menu: [
-      { category: "Pasta", items: [{ title: "Aglio e Olio" }, { title: "Carbonara" }] },
-      { category: "Ice cream", items: [{ title: "Chocolate" }, { title: "Vanillia" }] },
+      { category: "Hamburgers", items: [{ title: "Cheeseburger" }, { title: "Beefy" }, { title: "Double Patty" }, { title: "Chilli Cheese" }, { title: "Baconator" }, { title: "Mushroom Swiss Burger" }] },
+      { category: "Desserts", items: [{ title: "Apple Pie" }, { title: "Chocolate Cake" }, { title: "Carrot Cake" }, { title: "Lemon Bars" }, { title: "Brownie Sundae" }, { title: "Cinnamon Twists" }] },
     ],
   },
   {
@@ -53,8 +57,10 @@ let restaurantData = [
     pricing: "$$",
     img: brickImage,
     menu: [
-      { category: "Pasta", items: [{ title: "Aglio e Olio" }, { title: "Carbonara" }] },
-      { category: "Ice cream", items: [{ title: "Chocolate" }, { title: "Vanillia" }] },
+      { category: "Pasta", items: [{ title: "Spaghetti Bolognese" }, { title: "Fettuccine Alfredo" }, { title: "Penne alla Vodka" }, { title: "Lasagna" }, { title: "Penne alla Arrabbiata" }, { title: "Carbonara" }, { title: "Shrimp Scampi" }] },
+      { category: "Sweets", items: [{ title: "Tiramisu" }, { title: "Banana Split" }, { title: "Rice Pudding" }, { title: "Brownies" }, { title: "Panna Cotta" }, { title: "Cheesecake" }, { title: "Chocolate Fondue" }] },
+      { category: "Salads", items: [{ title: "Caesar Salad" }, { title: "Greek Salad" }, { title: "Caprese Salad" }, { title: "Cobb Salad" }] },
+      { category: "Seafood", items: [{ title: "Grilled Salmon" }, { title: "Shrimp Scampi" }, { title: "Lobster Tail" }, { title: "Fish Tacos" }] },
     ],
   },
   {
@@ -65,8 +71,9 @@ let restaurantData = [
     pricing: "$$",
     img: oldImage,
     menu: [
-      { category: "Pasta", items: [{ title: "Aglio e Olio" }, { title: "Carbonara" }] },
-      { category: "Ice cream", items: [{ title: "Chocolate" }, { title: "Vanillia" }] },
+      { category: "Noodles", items: [{ title: "Lo Mein" }, { title: "Chow Mein" }, { title: "Dandan Noodles" }, { title: "Sichuan Cold Noodles" }] },
+      { category: "Rice", items: [{title: "Yangzhou Fried Rice"}, {title: "Bao Zai Fan"}, {title: "Zong Zi"}]},
+      { category: "Deserts", items: [{ title: "Fried Ice Cream" }, { title: "Tangyuan" }, { title: "Egg Tarts" }, { title: "Nian Gao" }, { title: "Gui Ling Gao" }] }
     ],
   },
   {
@@ -77,8 +84,11 @@ let restaurantData = [
     pricing: "$",
     img: starbucksImage,
     menu: [
-      { category: "Pasta", items: [{ title: "Aglio e Olio" }, { title: "Carbonara" }] },
-      { category: "Ice cream", items: [{ title: "Chocolate" }, { title: "Vanillia" }] },
+      { category: "Coffee", items: [{ title: "Espresso" }, { title: "Cappuccino" }, { title: "Latte" }, { title: "Americano" }, { title: "Macchiato" }] },
+      { category: "Tea", items: [{ title: "Green Tea" }, { title: "Chai Tea" }, { title: "Earl Grey" }, { title: "Peppermint Tea" }] },
+      { category: "Pastries", items: [{ title: "Croissant" }, { title: "Danish Pastry" }, { title: "Cinnamon Roll" }, { title: "Muffin" }] },
+      { category: "Sandwiches", items: [{ title: "Turkey Club" }, { title: "Caprese Panini" }, { title: "Chicken Avocado Wrap" }] },
+      { category: "Desserts", items: [{ title: "Chocolate Brownie" }, { title: "Cheesecake" }, { title: "Tiramisu Cup" }, { title: "Fruit Parfait" }] },
     ],
   },
 ];
@@ -115,7 +125,6 @@ const HomescreenCell = ({ navigation, ...props }) => (
 
 const RestaurantsScreen = ({ navigation }) => (
   <View style={styles.restaurants_container}>
-    <Text>Restaurants Screen</Text>
     <ScrollView>
       <TableView>
         {/* iterate through restaurantData */}
@@ -134,7 +143,6 @@ const MenuScreen = ({ route }) => {
 
   return (
     <View style={styles.menu_container}>
-      <Text>Menu Screen</Text>
       <ScrollView>
         <TableView style={styles.menu_view}>
           {items.map((category, index) => (
