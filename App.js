c0873f2f457cefe09b9b24a25e928a0fd5b7b070
AndrejaTorkar <104751136+AndrejaTorkar@users.noreply.github.com>
2023-12-31 14:19:01 +0100

    navi styles fixed

--- App.js
+++ App.js
@@ -12,6 +12,9 @@ CODING RESOURCES:
 * https://legacy.reactjs.org/docs/components-and-props.html
 * https://colorhunt.co/palette/2b3499ff6c22ff9209ffd099
 *
+
+IMPROVEMENTS:
+*
 */
 
 import React from "react";
@@ -21,6 +24,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { TableView, Section, Cell } from "react-native-tableview-simple";
 import restaurantData from "./Restaurants_data";
 import { styles } from "./styles";
+import { field_height, field_width } from "./constants";
 
 const Stack = createStackNavigator();
 
@@ -28,10 +32,14 @@ const HomescreenCell = ({ navigation, ...props }) => (
   <Cell
     {...props}
     style={styles.rest_container}
+    title={<Text style={{...styles.menu_item_text_size, fontWeight: "bold"}}>{props.title}</Text>}
     onPress={() =>
-      navigation.navigate("Menu", {
+      navigation.navigate("Menu", 
+      {
         items: props.menu,
-      })
+      }
+            
+      )
     }
   >
     <Image source={props.img} style={styles.image} />
@@ -49,12 +57,27 @@ const HomescreenCell = ({ navigation, ...props }) => (
           <Text style={styles.additionalInfoTextBadge}>{props.pricing}</Text>
         </View>
         <View style={styles.badge}>
-          <Text style={{...styles.additionalInfoTextBadge, color: "green"}}>{props.delivery_time}</Text>
+          <Text style={styles.additionalInfoTextBadge}>{props.delivery_time}</Text>
           </View>
       </View>  
 
     </View>
   </Cell>
+);
+
+const RestaurantsScreen = ({ navigation }) => (
+  <View style={styles.restaurants_container}>
+    <ScrollView>
+      <TableView>
+        {/* iterate through restaurantData */}
+        {restaurantData.map((data, index) => (
+          <Section key={index} header="" sectionTintColor={"#FFEAC7"}>
+            <HomescreenCell navigation={navigation} {...data}/>
+          </Section>
+        ))}
+      </TableView>
+    </ScrollView>
+  </View>
 );
 
 const FoodInformationScreen = ({ route }) => {
@@ -63,30 +86,22 @@ const FoodInformationScreen = ({ route }) => {
   return (
     <View style={styles.menu_container}>
       <ScrollView>
-        <Text style={styles.additionalInfoText}>Title: {title}</Text>
-        <Text style={styles.additionalInfoText}>Price: ${price}</Text>
-        <Text style={styles.additionalInfoText}>Calories: {calories}</Text>
-        <Text style={styles.additionalInfoText}>Health Index: {healthIndex}</Text>
-        {/* Add more information as needed */}
+        <View style={styles.dish_title_field}>
+          <Text style={styles.dish_title}>{title}</Text>
+        </View>
+        <View style={styles.dish_price_field}>
+          <Text style={styles.dish_price}>Price: ${price}</Text>
+        </View>
+        <View style={styles.dish_calories_field}>
+          <Text style={styles.dish_calories}>Calories: {calories}</Text>
+        </View>
+        <View style={styles.dish_health_field}>
+          <Text style={styles.dish_health}>Health Index: {healthIndex}</Text>
+        </View>
       </ScrollView>
     </View>
   );
 };
-
-const RestaurantsScreen = ({ navigation }) => (
-  <View style={styles.restaurants_container}>
-    <ScrollView>
-      <TableView>
-        {/* iterate through restaurantData */}
-        {restaurantData.map((data, index) => (
-          <Section key={index} header="" separatorTintColor="blue" sectionTintColor={"red"}>
-            <HomescreenCell navigation={navigation} {...data} />
-          </Section>
-        ))}
-      </TableView>
-    </ScrollView>
-  </View>
-);
 
 const MenuScreen = ({ route, navigation }) => {
   const { items } = route.params;
@@ -94,9 +109,9 @@ const MenuScreen = ({ route, navigation }) => {
   return (
     <View style={styles.menu_container}>
       <ScrollView>
-        <TableView style={styles.menu_view}>
+        <TableView>
           {items.map((category, index) => (
-            <Section key={index} header={category.category}>
+            <Section key={index} header={<Text style={styles.menu_item_text_size}>{category.category}</Text>}>
               {category.items.map((item, itemIndex) => (
                 <TouchableOpacity
                   key={itemIndex}
@@ -109,7 +124,7 @@ const MenuScreen = ({ route, navigation }) => {
                     })
                   }
                 >
-                  <Cell title={item.title} />
+                  <Cell title={<Text style={styles.menu_item_text_size}>{item.title}</Text>} />
                 </TouchableOpacity>
               ))}
             </Section>
@@ -123,10 +138,21 @@ const MenuScreen = ({ route, navigation }) => {
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Restaurants" component={RestaurantsScreen} />
-        <Stack.Screen name="Menu" component={MenuScreen} />
+    <NavigationContainer style={{fontSize: 150}}>
+      <Stack.Navigator 
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#FFEAC7', 
+          },
+          headerTintColor: '#2B3499',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+            fontSize: field_width * 0.05,
+          },
+        }}
+      >
+        <Stack.Screen name="Restaurants" component={RestaurantsScreen}/>
+        <Stack.Screen name="Menu" component={MenuScreen}/>
         <Stack.Screen name="Food Information" component={FoodInformationScreen} />
       </Stack.Navigator>
     </NavigationContainer>
--- styles.js
+++ styles.js
@@ -11,41 +11,47 @@ export const styles = StyleSheet.create({
     height: 500,
     flexDirection: "column", // Adjust layout to column
     alignItems: "center", // Center items horizontally
+    backgroundColor: "#FFEAC7",
   },
   menu_container: {
-    alignContent: "stretch",
+    flex: 1,
   },
 
   additionalInfo: {
+    flex: 1,
     flexDirection: "row",
-    marginTop: 10,
+    marginTop: field_width * 0.02,
     justifyContent: "space-between",
-    paddingBottom: 10,
+    paddingBottom: field_height * 0.01,
+    paddingRight: field_width * 0.02,
   },
 
   image: {
     width: "100%",
-    height: 200,
+    height: field_width * 0.5,
     resizeMode: "cover", // Keep aspect ratio while covering the entire area
-    marginBottom: 10,
+    marginBottom: field_height * 0.01,
   },
 
-  // Pricing and delivery time column
-  restaurant_badges: {
+  ////////////////
+  ////////////////
+  ////////////////
+
+  // Type and cousine column
+  additionalInfoCol: {
     flex: 1,
-    flexDirection: "row",
-    justifyContent: "center"
+    flexDirection: "column",
+    paddingLeft: field_width * 0.05,
   },
 
-  // For pricing and delivery time
-  badge: {
+  // Additional info background
+  additionalInfoBadge: {
     flex: 1,
-    borderColor: "#BBB",
-    borderWidth: 2,
-    borderRadius: field_height * 0.02,
-    padding: 5, 
-    margin: 3,
-    alignContent: "center"
+    margin: 2,
+    backgroundColor: "#6F74CC",
+    width: field_width * 0.3,
+    borderRadius: field_height * 0.006,
+    justifyContent: "center",
   },
 
   // For restaurant's additional info
@@ -53,47 +59,98 @@ export const styles = StyleSheet.create({
     fontSize: field_width * 0.04,
     color: "#555",
     color: "white",
-    textAlign: "center"
-
+    textAlign: "center",
   },
 
-  // Additional info background
-  additionalInfoBadge: {
-    margin: 2,
-    backgroundColor: "#6F74CC",
-    width: field_width * 0.3,
-    borderRadius: 5,
+  ////////////////
+  ////////////////
+  ////////////////
+  // Pricing and delivery time column
+  restaurant_badges: {
+    flex: 1,
+    flexDirection: "row",
+  },
+
+  // For pricing and delivery time
+  badge: {
+    flex: 1,
+    backgroundColor: "#FFD099",
+    borderRadius: field_height * 0.02,
+    margin: field_height * 0.003,
+    justifyContent: "center",
   },
 
   // Text for pricing and delivery time
   additionalInfoTextBadge: {
-    color : "red",
+    color: "black",
     fontWeight: "bold",
-    textAlign: "center"
+    textAlign: "center",
+    fontSize: field_width * 0.03
   },
 
-  // Type and cousine column
-  additionalInfoCol: {
+  // Styles for food info
+  dish_title_field: {
     flex: 1,
-    flexDirection: "column",
-    paddingLeft: 20,
+    padding: field_width * 0.05,
+    alignItems: "center",
   },
-  
+  dish_title: {
+    color: "#2B3499",
+    fontSize: field_width * 0.07,
+    fontWeight: "bold"
+  },
+
+  dish_price_field: {
+    flex: 1,
+    alignItems: "center",
+    paddingBottom: field_width * 0.03
+
+  },
+  dish_price: {
+    color: "#6F74CC",
+    fontSize: field_width * 0.05
+  },
+
+  dish_calories_field: {
+    flex: 1,
+    alignItems: "center",
+    paddingBottom: field_width * 0.03
+  },
+
+  dish_calories: {
+    color: "#6F74CC",
+    fontSize: field_width * 0.05
+  },
+
+  dish_health_field: {
+    flex: 1,
+    alignItems: "center",
+  },
+
+  dish_health: {
+    color: "#2B3499",
+    fontSize: field_width * 0.05
+
+  },
+
+  menu_item_text_size: {
+    fontSize: field_width * 0.04
+  },
 
   // Normal
-    // #2B3499
-    // #FF6C22
-    // #FF9209
-    // #FFD099
+  // #2B3499
+  // #FF6C22
+  // #FF9209
+  // #FFD099
 
-    // Medium
-//    #6F74CC
-//    #FF9860
-//    #FFB956
-//    #FFEDC8
+  // Medium
+  //    #6F74CC
+  //    #FF9860
+  //    #FFB956
+  //    #FFEDC8
   // Pastel
-    // #DCE0FF
-    // #FFB09E
-    // #FFD4A4
-    // #FFEAC7
+  // #DCE0FF
+  // #FFB09E
+  // #FFD4A4
+  // #FFEAC7
 });
